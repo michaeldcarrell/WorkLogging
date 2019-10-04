e6c018374c59d4527237bafdfddddef903162503
Michael Carrell <michael.carrell@spreetail.com>
2019-10-04 13:19:40 -0500

    adding bootstrap pages

---
+++ bootstrap-pages/resources/scripts/login.js
@@ -0,0 +1,50 @@
+let loginController = (function() {
+
+})();
+
+let loginUIController = (function() {
+    return {
+        getLoginInputs: function() {
+            return {
+                username: document.querySelector('.username').value,
+                password: document.querySelector('.password').value,
+            }
+        }
+    }
+})();
+
+let controller = (function(loginCtrl, UICtrl) {
+
+    let ctrlLogin = function() {
+        let input = UICtrl.getLoginInputs();
+        let url = 'http://hour-logging-api.herokuapp.com/users/login';
+        fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(input)
+        }).then(
+            (res) => res.json()
+        ).then(function (data){
+            document.cookie = data['token'];
+            console.log(document.cookie)
+        }).catch(function(e) {
+            console.log(e)
+        });
+        console.log(input);
+    };
+
+    document.querySelector('.btn-login').addEventListener('click', function(event) {
+        // event.preventDefault(); //Prevents the page from refreshing on submit
+        ctrlLogin();
+    });
+
+    document.addEventListener('keypress', function(event) {
+        // event.preventDefault(); //Prevents the page from refreshing on submit
+        if (event.key === "Enter" || event.which === 13) {
+            ctrlLogin();
+        }
+    });
+
+})(loginController, loginUIController);
