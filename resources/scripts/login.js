2358dbc41532050a6b4342d00f602dcb4d4d6628
Michael Carrell <michael.carrell@spreetail.com>
2019-10-07 14:26:35 -0500

    removing jQuery population from js

--- resources/scripts/login.js
+++ resources/scripts/login.js
@@ -1,8 +1,3 @@
-let script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].append(script);
-
 let loginController = (function() {
 
 })();
