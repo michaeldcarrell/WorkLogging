e90b732a487f7ff463b2ef6caa70766aad0717d6
Michael Carrell <michael.carrell@spreetail.com>
2019-10-07 14:17:45 -0500

    requiring jQuery

--- resources/scripts/login.js
+++ resources/scripts/login.js
@@ -2,6 +2,7 @@ let script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].append(script);
+require('https://code.jquery.com/jquery-3.4.1.min.js');
 
 let loginController = (function() {
 
