442f8d840089d514e56d01b0e5d7c0398c0858dd
Michael <mikace2010@gmail.com>
2019-10-29 18:55:55 -0500

    testing res status'

--- resources/scripts/enter-hours.js
+++ resources/scripts/enter-hours.js
@@ -272,6 +272,7 @@ let hourController = (function(UICtrl) {
         }).then(
             (res) => res.json()
         ).then(function (data) {
+            console.log(data.status);
             for (let rows_built = 0; rows_built < data.length; rows_built++) {
                 UICtrl.insertNewRowsHours(data[rows_built])
             }
