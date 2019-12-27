ba004267f1f78d6bbcd76677aaf709ad7becaafc
michaeldcarrell <michaeldcarrell@gmail.com>
2019-12-27 08:19:48 -0800

    adding status and spinner

---
+++ resources/scripts/account.js
@@ -0,0 +1,64 @@
+let denyLogin = function() {
+    document.cookie = '';
+    // document.location.href = 'login'; instead this should pop up modal informing user something went wrong and to re-log
+};
+if (document.cookie === '') {
+    denyLogin()
+}
+
+let accountUIController = (function() {
+    return {}
+})();
+
+let hourController = (function(UICtrl) {
+
+    let logOutUser = () => {
+        let url = 'https://hour-logging-api.herokuapp.com/users/logout';
+        fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + document.cookie,
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': 'true'
+            }
+        }).then(function(res) {
+            console.log(res.json())
+        }).catch(function(e) {
+            console.log(e)
+        });
+    };
+
+    document.getElementById('account-action-logout').addEventListener('click', function(event) {
+        logOutUser();
+        document.cookie = '';
+        document.location.href = 'login';
+    });
+
+
+    let initAccountDetails = async () => {
+        let userDropDown = document.getElementById('account-option-dropdown');
+        let url = 'https://hour-logging-api.herokuapp.com/users/me';
+        fetch(url, {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + document.cookie,
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': 'true'
+            }
+        }).then(
+            (res) => res.json()
+        ).then(function(data) {
+            userDropDown.innerHTML = data['username'];
+        }).catch(function(e) {
+            console.log(e);
+            userDropDown.innerHTML = '';
+        })
+    };
+
+    let init = function() {
+        initAccountDetails();
+    }();
+
+})(accountUIController);
--- resources/scripts/login.js
+++ resources/scripts/login.js
@@ -14,7 +14,21 @@ let loginUIController = (function() {
 })();
 
 let controller = (function(loginCtrl, UICtrl) {
+    let spinInit = function() {
+        let spinner = new Spinner(opts).spin(target);
+        let body = document.getElementById('body-container');
+        body.style.display = 'block';
+        return spinner
+    };
+
+    let spinStop = function(spinner) {
+        let body = document.getElementById('body-container');
+        body.style.display = 'none';
+        spinner.stop();
+    };
+
     let ctrlLogin = function() {
+        let activeSpinner = spinInit();
         let input = UICtrl.getLoginInputs();
         let url = 'https://hour-logging-api.herokuapp.com/users/login';
         fetch(url, {
@@ -23,18 +37,20 @@ let controller = (function(loginCtrl, UICtrl) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(input)
-        }).then(
-            (res) => res.json()
-        ).then(function (data){
-            document.cookie = data['token'];
+        }).then(async (res) => {
+                return {
+                    status: res.status,
+                    body: await res.json()
+                }
+        }).then(async (data) => {
+            console.log(data.status);
+            document.cookie = data.body['token'];
             console.log(document.cookie);
-
-            document.location.href = 'enter-hours'
+            // document.location.href = 'enter-hours'
+            spinStop(activeSpinner);
         }).catch(function(e) {
             console.log('Login Failed');
-            // document.getElementById('loginFailedModal').style.display = 'block';
-            // document.getElementById('loginFailedModal').classList.add('show');
-            // document.body.classList.add('modal-open')
+            spinStop(activeSpinner);
             $('#loginFailedModal').modal('toggle');
         });
     };
